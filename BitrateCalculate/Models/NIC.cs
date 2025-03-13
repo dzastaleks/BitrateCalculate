3a12d8dece316c1953d22d19d06d68d71b1e8d30
Aleksandar Stupar <stupar94@gmail.com>
2025-03-13 23:13:01 +0100

    Add Models

---
+++ BitrateCalculate/Models/NIC.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitrateCalculate.Models
+{
+    public class NIC
+    {
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("MAC")]
+        public string Mac { get; set; }
+
+        [JsonProperty("Timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty("Rx")]
+        public long Rx { get; set; }
+
+        [JsonProperty("Tx")]
+        public long Tx { get; set; }
+    }
+}
---
+++ BitrateCalculate/Models/ResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitrateCalculate.Models
+{
+    public class ResponseModel
+    {
+        [JsonProperty("Device")]
+        public string Device { get; set; }
+
+        [JsonProperty("Model")]
+        public string Model { get; set; }
+
+        [JsonProperty("NIC")]
+        public List<NIC> Nic { get; set; }
+    }
+}
